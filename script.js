aa5b99137bbe4e5b1422b553bbeddbad1dfecce9
Jerry Neequaye <listonneequaye@gmail.com>
2026-09-02 23:06:50 -0500

    effectivechange
    now!

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+
+document.write("Ask me a question. I'll try to answer it in the <strong>comment.</strong><br>");
+
+document.write("My name is <strong>Jerry Neequaye</strong><br>");
+
+document.write("<img src='program.jpg' alt='program' width='400'>");
