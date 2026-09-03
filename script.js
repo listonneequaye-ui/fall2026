b450033bf4d7821cee4af08a7f5cd633ea24776a
Jerry Neequaye <listonneequaye@gmail.com>
2026-09-02 23:32:26 -0500

    more code
    effective now!!!

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ document.write("Ask me a question. I'll try to answer it in the <strong>comment.
 document.write("My name is <strong>Jerry Neequaye</strong><br>");
 
 document.write('<img src="program.jpg">');
+
+function showInformation() {
+    document.getElementById("message").innerHTML =
+        "Welcome! This information was created with JavaScript.";
+}
