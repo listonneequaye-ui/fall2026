e148d971ef2ce375cd7620fc5f950d9cc01dea8a
Jerry Neequaye <listonneequaye@gmail.com>
2026-09-02 23:22:42 -0500

    hj,lhj
    hjkhjkj

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ document.write("Ask me a question. I'll try to answer it in the <strong>comment.
 
 document.write("My name is <strong>Jerry Neequaye</strong><br>");
 
-document.write("<img src='program.jpg'>");
+document.write('<img src="program.jpg">');
